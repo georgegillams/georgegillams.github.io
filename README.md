754d1de61ac88e748007a46f1cdd4835585b3a1f
George333123 <georgegillams@hotmail.co.uk>
2017-12-04 11:59:37 +0000

    deploy

--- README.md
+++
@@ -1,1 +0,0 @@
-# georgegillams.github.io
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'bpk-stylesheets';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
