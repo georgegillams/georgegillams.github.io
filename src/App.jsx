aebffe1e1e24532dfb07e8a197976e0fdd6873a0
George333123 <georgegillams@hotmail.co.uk>
2017-12-06 11:28:12 +0000

    Fix es-lint warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BpkText from 'bpk-component-text';
-import BpkBannerAlert, { ALERT_TYPES } from 'bpk-component-banner-alert';
-// import * as TOKENS from 'bpk-tokens/tokens/ios/base.react.native.es6';
 import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid';
 import BpkButton from 'bpk-component-button';
-import MainContent from './MainContent';
 import GetSocial from './GetSocial';
 import NavLayout from './NavLayout';
 
 import STYLES from './app.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
-const MAX_BYTES_TO_HOLD_IN_MEMORY = 240;
 
-// const {
-//   animationDurationBase,
-// } = TOKENS;
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   comingSoon: true,
-    // };
-
-    // <editor-fold> Function Bindings
-    this.downloadPdf = this.downloadPdf.bind(this);
-    // </editor-fold> Function Bindings
-  }
-
-  // <editor-fold> State Setters
-  // onDataPatternChanged(event) {
-  //   this.setState({ dataPattern: event.target.value });
-  // }
-  // </editor-fold> State Setters
-
-  // <editor-fold> Helper Methods
-  downloadPdf() {
-    const fileContent = `data:text/txt;charset=utf-8,${'PDF Coming soon!'}`;
-    const encodedUri = encodeURI(fileContent);
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', 'george_gillams_no_pdf.txt');
-    document.body.appendChild(link); // Required for FF
-    link.click(); // This will download the data file named "configuration.json".
-  }
-  // </editor-fold> Helper Methods
-
-  render() {
-    return (
-      <div>
-        <header className={`${getClassName('app__header')} ${getClassName('app--centered')}`}>
-          <BpkGridContainer>
-            <BpkGridRow>
-              <BpkGridColumn width={12}>
-                <BpkText tagName="h1" textStyle="xxl" className={getClassName('app__text')}>George Gillams</BpkText>
-                <BpkText tagName="h3" textStyle="md" className={getClassName('app__text')}>Open-source Software Engineer</BpkText>
-              </BpkGridColumn>
-            </BpkGridRow>
-          </BpkGridContainer>
-        </header>
-        <BpkButton link onClick={this.downloadPdf} className={getClassName('app__download-button')}>Download CV</BpkButton>
-        <GetSocial />
-        <NavLayout />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header className={`${getClassName('app__header')} ${getClassName('app--centered')}`}>
+      <BpkGridContainer>
+        <BpkGridRow>
+          <BpkGridColumn width={12}>
+            <BpkText tagName="h1" textStyle="xxl" className={getClassName('app__text')}>George Gillams</BpkText>
+            <BpkText tagName="h3" textStyle="md" className={getClassName('app__text')}>Open-source Software Engineer</BpkText>
+          </BpkGridColumn>
+        </BpkGridRow>
+      </BpkGridContainer>
+    </header>
+    <a href="https://dl.dropbox.com/s/smcdcgitd4nog17/George%20Gillams%20CV.pdf?dl=0">
+      <BpkButton link onClick={null} className={getClassName('app__download-button')}>Download CV</BpkButton>
+    </a>
+    <GetSocial />
+    <NavLayout />
+  </div>
+);
 
 export default App;
--- src/NavLayout.jsx
+++ src/NavLayout.jsx
@@ -1,51 +1,22 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import { BpkCode, BpkCodeBlock } from 'bpk-component-code';
-import BpkInput from 'bpk-component-input';
-import BpkButton from 'bpk-component-button';
-import BpkText from 'bpk-component-text';
-import AnimateHeight from 'bpk-animate-height';
-import BpkBannerAlert, { ALERT_TYPES } from 'bpk-component-banner-alert';
-import BpkCheckBox from 'bpk-component-checkbox';
-import ReactFileReader from 'react-file-reader';
-import { colors } from 'bpk-tokens/tokens/base.es6';
-import { alignToButton } from 'bpk-component-icon';
-import BpkSmallDownloadIcon from 'bpk-component-icon/sm/download';
-import BpkSmallUpgradeIcon from 'bpk-component-icon/sm/upgrade';
-// import * as TOKENS from 'bpk-tokens/tokens/ios/base.react.native.es6';
-import BpkHorizontalNav, { BpkHorizontalNavItem } from 'bpk-component-horizontal-nav';
-import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid';
-import MainContent from './MainContent';
 import ProfilePic from './ProfilePic';
 import ComingSoon from './ComingSoon';
-import GetSocial from './GetSocial';
 
 import STYLES from './nav-layout.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
-const AlignedBpkSmallDownloadIcon = alignToButton(BpkSmallDownloadIcon);
-const AlignedBpkSmallUpgradeIcon = alignToButton(BpkSmallUpgradeIcon);
 
-const NavLayout = props =>
-
-  // TODO MAKE THIS RESPONSIVE: ie HIDE NAV COL IFF < tablet breakpoint
-
-  (
-    <div className={getClassName('nav-layout__main')}>
-      <div className={getClassName('nav-layout__centre-column')}>
-        <span className={getClassName('nav-layout__nav-col')}>
-          <ProfilePic />
-        </span>
-        <span className={getClassName('nav-layout__content-col')}>
-          <ComingSoon />
-        </span>
-      </div>
+const NavLayout = () => (
+  <div className={getClassName('nav-layout__main')}>
+    <div className={getClassName('nav-layout__centre-column')}>
+      <span className={getClassName('nav-layout__nav-col')}>
+        <ProfilePic />
+      </span>
+      <span className={getClassName('nav-layout__content-col')}>
+        <ComingSoon />
+      </span>
     </div>
-  )
-;
-
-// NavLayout.propTypes = {
-//   // serialPortPath: PropTypes.string.isRequired,
-// };
+  </div>
+);
 
 export default NavLayout;
