67ddae344063b12bc87c122100d22ac580248e93
George333123 <georgegillams@hotmail.co.uk>
2017-12-22 15:43:48 +0000

    Home page redesign

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Section from '../components/Section';
+import SubSection from '../components/SubSection';
 
 import STYLES from './pages.scss';
 
@@ -7,22 +8,28 @@ const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 /* eslint-disable max-len */
 const HomePage = () => (
-  <main >
-    {/* <a className={getClassName('pages__link')} href="/vim">
-      <Section className={getClassName('pages__home-page-item')} name="Switching to Vim">
-	      I recently switched from Atom to Vim, and it was actually a fairly quick and pain-free process.
-      </Section>
-    </a> */}
-    {/* <a className={getClassName('pages__link')} href="/engagement">
+  <main className={getClassName('pages__home-page-tree')}>
+    <div>
+      <SubSection className={getClassName('pages__home-page-item')} name="Articles">
+        <a className={getClassName('pages__link')} href="/vim">
+Switching to Vim
+        </a>
+        <br />
+        <a className={getClassName('pages__link')} href="/net-neutrality">
+       My Take on Net Neutrality
+        </a>
+      </SubSection>
+      <SubSection className={getClassName('pages__home-page-item')} name="Travel">
+        <a className={getClassName('pages__link')} href="/Iceland">
+Iceland
+        </a>
+      </SubSection>
+      {/* <a className={getClassName('pages__link')} href="/engagement">
       <Section className={getClassName('pages__home-page-item')} name="We just got engaged!">
         I finally got around to asking, and she said yes!
       </Section>
     </a> */}
-    <a className={getClassName('pages__link')} href="/net-neutrality">
-      <Section className={getClassName('pages__home-page-item')} name="My Take on Net Neutrality">
-        In the US right now the FCC are getting ready for a vote to end Net-Neutrality, but many (myself included) believe that Ajit Pai is coordinating this brutal attack on the Internet just to line the pockets of his friends at Comcast, AT&amp;T and Verizon.
-      </Section>
-    </a>
+    </div>
   </main>
 );
 
