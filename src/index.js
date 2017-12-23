18395668bb166e4dea57cc023c2609c556ea7c5c
George333123 <georgegillams@hotmail.co.uk>
2017-12-23 22:46:18 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import WebFont from 'webfontloader';
 import App from './components/App';
-
-WebFont.load({
-  google: {
-    families: ['Quattrocento Sans:400,700'],
-  },
-});
 
 /* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(
