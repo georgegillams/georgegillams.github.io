72c6a358b4273291222212ed5576321446a61a88
George333123 <georgegillams@hotmail.co.uk>
2017-12-23 20:29:31 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Iceland:300,400,700', 'cursive'],
+  },
+});
 
 /* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(
