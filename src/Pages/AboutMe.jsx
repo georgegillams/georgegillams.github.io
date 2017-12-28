ad4e1e57a8d9f9ce9fe9bc0358e09a004492dafb
George333123 <georgegillams@hotmail.co.uk>
2017-12-28 12:55:57 +0000

    Fix typo

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -25,7 +25,7 @@ const AboutMe = (props) => {
           I&apos;m an open-source software engineer, passionate about design, travel and photography. When I&apos;m not internationally photobombing unsuspecting strangers I&apos;m based somewhere between Southampton and London.
         <br />
         <br />
-        I enjoy getting outdoors and trying new things, and will be completeing my 5th Tough Mudder in May 2018. I also try to squeeze in as much volunteering as I can, so you may find me dressed as a for Help for Heroes bear, reviewing content for Beat, or submitting PR's to Mozilla's open-source. I look forward to doing more of all that when I finish my Masters.
+        I enjoy getting outdoors and trying new things, and will be completeing my 5th Tough Mudder in May 2018. I also try to squeeze in as much volunteering as I can, so you may find me dressed as a Help for Heroes bear, reviewing content for Beat, or submitting PR's to Mozilla's open-source. I look forward to doing more of all that when I finish my Masters.
         <br />
         <br />
         {/* I recently got engaged
