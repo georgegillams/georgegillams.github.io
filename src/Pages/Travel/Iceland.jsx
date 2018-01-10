c65596318d460a1a050546d6ea168dac11cb8129
George333123 <georgegillams@hotmail.co.uk>
2018-01-10 18:04:08 +0000

    Add nav bar

--- src/Pages/Travel/Iceland.jsx
+++ src/Pages/Travel/Iceland.jsx
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import BpkImage, { withLazyLoading, withLoadingBehavior } from 'bpk-component-image';
 import Section from './../../components/Section';
 import SubSection from './../../components/SubSection';
-import iceland from './images/iceland.jpg';
+import iceland0 from './images/iceland0.jpg';
+import iceland1 from './images/iceland1.jpg';
+import iceland2 from './images/iceland2.jpg';
+import iceland3 from './images/iceland3.jpg';
 
 import STYLES from '../pages.scss';
 
@@ -22,7 +25,10 @@ const Iceland = (props) => {
       <Section name="Iceland  🛥 January 2018" >
         <a className={getClassName('pages__link')} href="https://flic.kr/s/aHskqS4GoZ" rel="noopener noreferrer" target="_blank">
           <SubSection name="See the full album on Flickr →" textClassName={getClassName('pages__link')} />
-          <FadingLazyLoadedImage className={getClassName('pages__image')} altText="Overlooking the harbour" width={2514} height={1676} src={iceland} />
+          <FadingLazyLoadedImage className={getClassName('pages__image')} altText="Overlooking the harbour" width={3} height={2} src={iceland0} />
+          <FadingLazyLoadedImage className={getClassName('pages__image')} altText="Overlooking the harbour" width={3} height={2} src={iceland1} />
+          <FadingLazyLoadedImage className={getClassName('pages__image')} altText="Overlooking the harbour" width={3} height={2} src={iceland2} />
+          <FadingLazyLoadedImage className={getClassName('pages__image')} altText="Overlooking the harbour" width={2} height={3} src={iceland3} />
         </a>
       </Section>
     </main>
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from 'reactstrap';
+
+import STYLES from './section.scss';
+
+const getClassName = className => STYLES[className] || 'UNKNOWN';
+
+import { colorGray700 } from 'bpk-tokens/tokens/base.es6';
+
+const linkStyle = { color: colorGray700, fontFamily: 'Neuton' };
+
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+    toggle = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+      const { className, ...rest } = this.props;
+      const outerClassNameFinal = [];
+      if (className) { outerClassNameFinal(className); }
+
+      return (
+        <div>
+          <Navbar light expand="sm" {...rest}>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle style={linkStyle} nav caret>
+                    Articles
+                  </DropdownToggle>
+                  <DropdownMenu >
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/articles/vim/">Switching to Vim</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/articles/net-neutrality/">My Take on Net Neutrality</NavLink>
+                    </NavItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle style={linkStyle} nav caret>
+                    Travel
+                  </DropdownToggle>
+                  <DropdownMenu >
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/travel/iceland-2018/">Iceland</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/travel/longleat-2017/">Longleat</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/travel/munich-2017/">Munich</NavLink>
+                    </NavItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                {/* <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle style={linkStyle} nav caret>
+                    Documents
+                  </DropdownToggle>
+                  <DropdownMenu >
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/documents/degree/">Degree Certificate</NavLink>
+                    </NavItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown> */}
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle style={linkStyle} nav caret>
+                    Other stuff
+                  </DropdownToggle>
+                  <DropdownMenu >
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/about">About me</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink style={linkStyle} href="/contact">Contact</NavLink>
+                    </NavItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+}
+
+NavigationBar.propTypes = {
+  className: PropTypes.string,
+};
+
+NavigationBar.defaultProps = {
+  className: null,
+};
+
+export default NavigationBar;
