23bf7c67fb092e3959fb1c13d87527f55bd0a681
George333123 <georgegillams@hotmail.co.uk>
2018-01-12 19:21:51 +0000

    Swap homepage iceland image

--- src/Pages/HomePageBanners/IcelandBanner.jsx
+++ src/Pages/HomePageBanners/IcelandBanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SimpleBanner from './SimpleBanner';
-import iceland from '../Travel/images/iceland0.jpg';
+import iceland from '../Travel/images/iceland2.jpg';
 
 const IcelandBanner = () => (
   <SimpleBanner imageSrc={iceland} linkUrl="/travel/iceland-2018" title="Iceland 2018" />
